fa3e083fd089105eb0ef6e51181419157cd8609c
paulsztwiertnia <102555250+paulsztwiertnia@users.noreply.github.com>
2025-02-04 22:26:06 -0500

    added content

--- src/app/content/index.ts
+++ src/app/content/index.ts
@@ -3,3 +3,12 @@ export const aboutMe = {
     description: "As a Software Engineer with a Bachelor of Science in Computer Science from Ryerson University, I'm passionate about creating scalable and user-centric web applications. My experience spans various industries (Government, B2B, B2C), where I have successfully led and been part of development teams and projects, specializing in delivering high-performance, responsive cloud and web solutions. I use Next.js and React.js to create dynamic, interactive, and high-performing web applications that are responsive, accessible, secure, and optimized for seamless user experiences across all devices. Over the past several years, I have successfully developed and maintained multiple WordPress sites, demonstrating expertise in customizing themes, optimizing performance, SEO, and ensuring seamless user experiences. My proficiency also extends to e-commerce integrations, where I have implemented both Shopify and WooCommerce solutions to create secure, scalable online stores. I have also built complete Shopify solutions from the ground up, showcasing versatility in delivering robust and engaging digital experiences. "
 };
 
+export const projects = [
+    {
+        title: "Strive Run Club",
+        description: "Strive Run Club is a community run, Etobicoke based running club for all levels of runners. Their website lets them stand out from other local running clubs and allows them to connect with runners in the community via events, training sessions, and group runs.",
+        image: "/projects/strive.png",
+        link: "https://www.google.com",
+        techStack: ["php.png", "javascript.png", "html.png", "css.png", "wordpress.png"]
+    }
+]
