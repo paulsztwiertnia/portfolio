594623edacd2353db884ddb4972b61a047285f80
paulsztwiertnia <102555250+paulsztwiertnia@users.noreply.github.com>
2025-02-04 22:25:38 -0500

    imported content and projects, changed styles

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,10 +5,11 @@ import JobTitle from '../components/ui/jobTitle';
 import Avatar from '@mui/material/Avatar';
 import { motion } from 'framer-motion';
 
-import { aboutMe } from './content';
+import { aboutMe, projects } from './content';
 import SocialLinks from '@/components/ui/socialLinks';
 import ScrollProgress from '@/components/ui/scrollProgress';
 import IconMotion from '@/components/ui/iconMotion';
+import Project from '@/components/ui/project';
 
 export default function Home() {
   return (
@@ -27,7 +28,7 @@ export default function Home() {
           >
             <Avatar 
               alt="Paul Sztwiertnia animated" 
-              src="/hero3.png" 
+              src="/icons/hero3.png" 
               className="shadow-lg" 
               sx={{ 
                 width: { xs: 200, sm: 250, md: 350, lg: 550 }, 
@@ -55,40 +56,40 @@ export default function Home() {
             <div className="max-w-xl">
               <h3 className="text-xl font-light">About Me</h3>
               <p className="text-3xl md:text-4xl font-light mb-2">Introduction.</p>
-              <p className="text-md font-light leading-loose">{aboutMe.description}</p>
+              <p className="text-lg font-light leading-loose font-open-sans">{aboutMe.description}</p>
             </div>
           <div className="max-w-xl pl-10">
             <div className="flex justify-start items-start py-2 mb-5 space-x-4">
               <div className="flex flex-col items-center">
-                <IconMotion src="/typescript.png" alt="Typescript" width={100} height={100} toolTip="Typescript" className="mb-8" yCord={[0, -20, 0]} />
-                <IconMotion src="/javascript.png" alt="Javascript" width={100} height={100} toolTip="Javascript" className="mb-8"  yCord={[0, -18, 0]} />
-                <IconMotion src="/c-programming.svg" alt="C" width={100} height={100} toolTip="C" className="mb-8" yCord={[0, -22, 0]} />
-                <IconMotion src="/python-logo.png" alt="Python" width={100} height={100} toolTip="Python" className="mb-8" yCord={[0, -19, 0]} />
-                <IconMotion src="/php.png" alt="PHP" width={100} height={100} toolTip="PHP" className="mb-8" yCord={[0, -21, 0]} />
+                <IconMotion src="/icons/typescript.png" alt="Typescript" width={100} height={100} toolTip="Typescript" className="mb-8" yCord={[0, -20, 0]} />
+                <IconMotion src="/icons/javascript.png" alt="Javascript" width={100} height={100} toolTip="Javascript" className="mb-8"  yCord={[0, -18, 0]} />
+                <IconMotion src="/icons/c-programming.svg" alt="C" width={100} height={100} toolTip="C" className="mb-8" yCord={[0, -22, 0]} />
+                <IconMotion src="/icons/python-logo.png" alt="Python" width={100} height={100} toolTip="Python" className="mb-8" yCord={[0, -19, 0]} />
+                <IconMotion src="/icons/php.png" alt="PHP" width={100} height={100} toolTip="PHP" className="mb-8" yCord={[0, -21, 0]} />
                 
               </div>
               <div className="flex flex-col items-center">
-                <IconMotion src="/react.png" alt="React" width={100} height={100} toolTip="React" className="mb-8" yCord={[0, -18, 0]} />
-                <IconMotion src="/nextJs.png" alt="Next.js" width={125} height={125} toolTip="Next.js" className="mb-8" yCord={[0, -20, 0]} />
-                <IconMotion src="/expressJs.png" alt="Express.js" width={100} height={100} toolTip="Express.js" className="mb-8" yCord={[0, -19, 0]} />
-                <IconMotion src="/nodeJs.png" alt="Node.js" width={100} height={100} toolTip="Node.js" className="mb-8" yCord={[0, -22, 0]} />
-                <IconMotion src="/nginx.svg" alt="Nginx" width={100} height={100} toolTip="Nginx" className="mb-8" yCord={[0, -21, 0]} />
+                <IconMotion src="/icons/react.png" alt="React" width={100} height={100} toolTip="React" className="mb-8" yCord={[0, -18, 0]} />
+                <IconMotion src="/icons/nextJs.png" alt="Next.js" width={125} height={125} toolTip="Next.js" className="mb-8" yCord={[0, -20, 0]} />
+                <IconMotion src="/icons/expressJs.png" alt="Express.js" width={100} height={100} toolTip="Express.js" className="mb-8" yCord={[0, -19, 0]} />
+                <IconMotion src="/icons/nodeJs.png" alt="Node.js" width={100} height={100} toolTip="Node.js" className="mb-8" yCord={[0, -22, 0]} />
+                <IconMotion src="/icons/nginx.svg" alt="Nginx" width={100} height={100} toolTip="Nginx" className="mb-8" yCord={[0, -21, 0]} />
 
               </div>
               <div className="flex flex-col items-center">
-                <IconMotion src="/html.png" alt="HTML" width={100} height={100} toolTip="HTML" className="mb-8" yCord={[0, -20, 0]} />
-                <IconMotion src="/css.png" alt="CSS" width={100} height={100} toolTip="CSS" className="mb-8" yCord={[0, -18, 0]} />
-                <IconMotion src="/tailwind.png" alt="Tailwind" width={100} height={100} toolTip="Tailwind" className="mb-8" yCord={[0, -22, 0]} />
-                <IconMotion src="/bootstrap.png" alt="Bootstrap" width={100} height={100} toolTip="Bootstrap" className="mb-8" yCord={[0, -19, 0]} />
-                <IconMotion src="/git.png" alt="Git" width={100} height={100} toolTip="Git" className="mb-8" yCord={[0, -21, 0]} />
+                <IconMotion src="/icons/html.png" alt="HTML" width={100} height={100} toolTip="HTML" className="mb-8" yCord={[0, -20, 0]} />
+                <IconMotion src="/icons/css.png" alt="CSS" width={100} height={100} toolTip="CSS" className="mb-8" yCord={[0, -18, 0]} />
+                <IconMotion src="/icons/tailwind.png" alt="Tailwind" width={100} height={100} toolTip="Tailwind" className="mb-8" yCord={[0, -22, 0]} />
+                <IconMotion src="/icons/bootstrap.png" alt="Bootstrap" width={100} height={100} toolTip="Bootstrap" className="mb-8" yCord={[0, -19, 0]} />
+                <IconMotion src="/icons/git.png" alt="Git" width={100} height={100} toolTip="Git" className="mb-8" yCord={[0, -21, 0]} />
 
               </div>
               <div className="flex flex-col items-center">
-              <IconMotion src="/shopify.png" alt="Shopify" width={100} height={100} toolTip="Shopify" className="mb-8" yCord={[0, -20, 0]} />
-              <IconMotion src="/craftCms.png" alt="Craft CMS" width={100} height={100} toolTip="Craft CMS" className="mb-8" yCord={[0, -18, 0]} />
-              <IconMotion src="/wordpress.png" alt="Wordpress" width={100} height={100} toolTip="Wordpress" className="mb-8" yCord={[0, -22, 0]} />
-              <IconMotion src="/firebase.png" alt="Firebase" width={100} height={100} toolTip="Firebase" className="mb-8" yCord={[0, -19, 0]} />
-              <IconMotion src="/mySql.png" alt="MySQL" width={100} height={100} toolTip="MySQL" className="mb-8" yCord={[0, -21, 0]} />
+              <IconMotion src="/icons/shopify.png" alt="Shopify" width={100} height={100} toolTip="Shopify" className="mb-8" yCord={[0, -20, 0]} />
+              <IconMotion src="/icons/craftCms.png" alt="Craft CMS" width={100} height={100} toolTip="Craft CMS" className="mb-8" yCord={[0, -18, 0]} />
+              <IconMotion src="/icons/wordpress.png" alt="Wordpress" width={100} height={100} toolTip="Wordpress" className="mb-8" yCord={[0, -22, 0]} />
+              <IconMotion src="/icons/firebase.png" alt="Firebase" width={100} height={100} toolTip="Firebase" className="mb-8" yCord={[0, -19, 0]} />
+              <IconMotion src="/icons/mySql.png" alt="MySQL" width={100} height={100} toolTip="MySQL" className="mb-8" yCord={[0, -21, 0]} />
               
               </div>
             </div>
@@ -98,24 +99,19 @@ export default function Home() {
       </div>
 
         {/* Projects Section */}
-        <div className="flex flex-row justify-center items-center min-h-90vh mt-divider">
+        <div className="flex flex-row justify-center items-center">
           <motion.div
               initial={{ opacity: 0, y: 20 }}
               whileInView={{ opacity: 1, y: 0 }}
               transition={{ duration: 0.8, delay: 0.2 }}
               viewport={{ once: true }}
           >
-            <div className="max-w-xl">
+            <div className="text-center">
               <h3 className="text-xl font-light">Projects</h3>
-              <p className="text-3xl md:text-4xl font-light mb-2">What I'm Working On.</p>
-              
+              <p className="text-3xl md:text-4xl font-light mb-2">Discover My Work.</p>
+              <Project title={projects[0].title} description={projects[0].description} image={projects[0].image} link={projects[0].link} techStack={projects[0].techStack} />
             </div>
           </motion.div>
-          <div className="max-w-xl pl-10">
-            <div className="flex justify-start items-start py-2 mb-5 space-x-4">
-              
-            </div>
-          </div>
         </div>
 
       <div className="pt-2">
