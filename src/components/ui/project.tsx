a35d47fa4644c3d9980b3cd0796ef2c29b7b634e
paulsztwiertnia <102555250+paulsztwiertnia@users.noreply.github.com>
2025-02-11 23:48:04 -0500

    classes changes to project component

--- src/components/ui/project.tsx
+++ src/components/ui/project.tsx
@@ -11,20 +11,20 @@ interface ProjectProps {
 
 const Project = ({ title, description, image, link, techStack }: ProjectProps) => {
     return (
-        <div className="flex flex-row justify-center mt-10 w-full">
-            <div className="flex flex-row justify-center w-5/6 rounded-md bg-slate-200">
-                <div className="flex flex-col justify-center items-center w-full py-8 ms-8">
+        <div className="flex flex-row justify-center mt-5 w-full rounded-md bg-gray-50">
+            <div className="flex flex-row justify-center w-full max-w-7xl">
+                <div className="flex flex-col justify-end items-end w-1/2 py-8 px-8">
                     <Image
                         src={image}
                         alt={title}
                         width={600}
                         height={600}
-                        className="rounded-md"
+                        className="rounded-md w-full h-auto"
                     />     
                 </div> 
-                <div className="flex flex-col justify-center items-start ml-10">
+                <div className="flex flex-col justify-center items-start w-1/2 px-4">
                     <h1 className="text-3xl font-bold mb-4">{title}</h1>
-                    <p className="text-sm text-left mb-2 font-open-sans w-5/6">{description}</p>
+                    <p className="text-sm text-left mb-2 font-open-sans">{description}</p>
                     <div className="flex flex-row items-center gap-2">
                         {techStack.map((tech, index) => (
                             <div key={index}>
@@ -32,12 +32,12 @@ const Project = ({ title, description, image, link, techStack }: ProjectProps) =
                             </div>
                         ))}
                     </div>
-                    {/* <button
-                        className="bg-primary text-white px-4 py-2 rounded-md text-xs hover:bg-primary-dark transition-colors"
+                    <button
+                        className="bg-primary text-white px-4 py-2 rounded-md text-xs hover:bg-primary-dark transition-colors mt-3"
                         onClick={() => window.open(link, '_blank')}
                     >
                         View Project
-                    </button> */}
+                    </button>
                 </div>
             </div>
         </div>
