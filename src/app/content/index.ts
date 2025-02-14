707f1997f42bde71f67f5a576b8204a83c38d9ed
paulsztwiertnia <102555250+paulsztwiertnia@users.noreply.github.com>
2025-02-13 22:15:39 -0500

    added content

--- src/app/content/index.ts
+++ src/app/content/index.ts
@@ -10,15 +10,7 @@ export const projects = [
         image: "/projects/novell.png",
         link: "https://www.novellsoftwaresolutions.com/",
         techStack: ["php.png", "javascript.png", "html.png", "css.png", "wordpress.png"],
-        color: "fff"
-    },
-    {
-        title: "Strive Run Club",
-        description: "Strive Run Club is a community run, Etobicoke based running club for all levels of runners. Their website lets them stand out from other local running clubs and allows them to connect with runners in the community via events, training sessions, and group runs.",
-        image: "/projects/strive.png",
-        link: "https://www.google.com",
-        techStack: ["php.png", "javascript.png", "html.png", "css.png", "wordpress.png"],
-        color: "lightgray"
+        color: "#FFF8DE"
     },
     {
         title: "Toronto Sweep & Scrub",
@@ -26,28 +18,47 @@ export const projects = [
         image: "/projects/tss.png",
         link: "https://www.torontosweepandscrub.com/",
         techStack: ["php.png", "javascript.png", "html.png", "css.png", "wordpress.png"],
-        color: "lightblue"
+        color: "#C5D3E8"
     },
     {
         title: "MYC Interactive",
         description: "MYC Interactive is a digital marketing agency specializing in web design, development, social media marketing, SEO, PPC advertising, and marketing strategy. With a dedicated team of experts, the company delivers comprehensive digital solutions designed to enhance brand visibility and drive business growth.",
         image: "/projects/myci.png",
         link: "https://www.mycinteractive.com/",
-        techStack: ["php.png", "javascript.png", "html.png", "css.png", "wordpress.png"]
+        techStack: ["php.png", "javascript.png", "html.png", "css.png", "wordpress.png"],
+        color: "#A6AEBF"
     },
     {
         title: "Market Your Car",
         description: "Market Your Car Inc. is a leading vehicle graphics and marketing company specializing in high-quality car, bus, truck, and trailer wraps. Established in 2008, the company has grown rapidly in the fleet wraps industry, operating from an 8,000 sq. ft. facility equipped to handle large-scale installations.",
         image: "/projects/myc.png",
         link: "https://www.marketyourcar.ca/",
-        techStack: ["php.png", "javascript.png", "html.png", "css.png", "wordpress.png"]
+        techStack: ["php.png", "javascript.png", "html.png", "css.png", "wordpress.png"],
+        color: "#F5EFFF"
     },
     {
         title: "Canadian Arab Federation",
         description: "Established in 1967, the Canadian Arab Federation (CAF) is a non-partisan, non-profit, and membership-based organization dedicated to representing Canadian Arabs on critical public policy issues.",
         image: "/projects/caf.png",
         link: "https://www.canadianarabsfederation.com",
-        techStack: ["php.png", "javascript.png", "html.png", "css.png", "wordpress.png"]
+        techStack: ["php.png", "javascript.png", "html.png", "css.png", "wordpress.png"],
+        color: "#EF9C66"
+    },
+    {
+        title: "Strive Run Club",
+        description: "Strive Run Club is a community run, Etobicoke based running club for all levels of runners. Their website lets them stand out from other local running clubs and allows them to connect with runners in the community via events, training sessions, and group runs.",
+        image: "/projects/strive.png",
+        link: "https://www.striverunclub.com",
+        techStack: ["php.png", "javascript.png", "html.png", "css.png", "wordpress.png"],
+        color: "#D0E8C5"
+    },
+    {
+        title: "Beata's Bakery",
+        description: "Located in Toronto's Bloor West Village, Beata crafts exceptional artisanal loaf cakes available through her secure online store. Her signature creations feature real fruit and a touch of alcohol, delivering a unique flavor experience to customers across the city.",
+        image: "/projects/beata.png",
+        link: "https://www.beatasbakery.ca",
+        techStack: ["php.png", "javascript.png", "html.png", "css.png", "wordpress.png"],
+        color: "#FAEDCE"
     }
 
 ]
