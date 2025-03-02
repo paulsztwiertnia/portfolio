7e207f3a839a8836c487cc3791f6289679ab44b5
paulsztwiertnia <102555250+paulsztwiertnia@users.noreply.github.com>
2025-03-01 19:22:30 -0500

    responsive adjsutments

--- src/components/ui/socialLinks.tsx
+++ src/components/ui/socialLinks.tsx
@@ -3,10 +3,10 @@ import { GitHub } from '@mui/icons-material';
 
 const SocialLinks = () => {
   return (
-    <div className="flex justify-center items-center space-x-4">
-      <button className="bg-primary text-white px-4 py-2 rounded-md text-xs hover:bg-primary-dark transition-colors">My Resume</button>
-      <LinkedIn fontSize="large" className="cursor-pointer hover:text-primary transition-colors" />
-      <GitHub fontSize="large" className="cursor-pointer hover:text-primary transition-colors" />
+    <div className="flex justify-center items-center space-x-4 mt-4">
+      <button className="bg-primary text-white px-3 py-1.5 md:px-4 md:py-2 rounded-md text-xs hover:bg-primary-dark transition-colors">My Resume</button>
+      <LinkedIn fontSize="medium" className="cursor-pointer hover:text-primary transition-colors" />
+      <GitHub fontSize="medium" className="cursor-pointer hover:text-primary transition-colors" />
     </div>
   )
 }
