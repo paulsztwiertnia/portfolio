38fe68ef7b9598c07b531c151e5e224b3041752d
paulsztwiertnia <102555250+paulsztwiertnia@users.noreply.github.com>
2025-03-04 22:17:39 -0500

    styling & responsive changes

--- src/components/ui/header.tsx
+++ src/components/ui/header.tsx
@@ -1,22 +1,21 @@
-import * as React from 'react';
-import { useState } from 'react';
 import Link from 'next/link';
 import Image from 'next/image';
 import Hamburger from './hamburger';
 
 export default function Header() {
-  const [value, setValue] = React.useState('home');
-
-  const handleChange = (event: React.SyntheticEvent, newValue: string) => {
-    setValue(newValue);
-  };
 
   return (
     <>
-    <div className="flex justify-between items-center sticky top-0 z-50">
-      <div className="flex items-center px-2">
+    <div className="flex justify-between items-center sticky top-0 z-50 bg-inherit h-[72px]">
+      <div className="flex items-center px-2 py-2">
         <Link href="/">
-          <Image src="/icons/PS-logo-transparent.png" alt="Logo" className="w-16 h-16 mt-2 me-3" width={100} height={100} />
+          <Image 
+            src="/icons/PS-logo-transparent.png" 
+            alt="Logo" 
+            className="w-10 h-10 xs:w-12 xs:h-12 sm:w-16 sm:h-16 md:w-16 md:h-16 mt-2 me-3 absolute top-2 left-2" 
+            width={100} 
+            height={100} 
+          />
         </Link>
       </div>
       
