cee8b53dd32ee1a5c064905f244e2add5a8546a8
paulsztwiertnia <102555250+paulsztwiertnia@users.noreply.github.com>
2025-03-04 22:17:54 -0500

    added links

--- src/components/ui/socialLinks.tsx
+++ src/components/ui/socialLinks.tsx
@@ -4,9 +4,13 @@ import { GitHub } from '@mui/icons-material';
 const SocialLinks = () => {
   return (
     <div className="flex justify-center items-center space-x-4 mt-4">
-      <button className="bg-primary text-white px-3 py-1.5 md:px-4 md:py-2 rounded-md text-xs hover:bg-primary-dark transition-colors">My Resume</button>
-      <LinkedIn fontSize="medium" className="cursor-pointer hover:text-primary transition-colors" />
-      <GitHub fontSize="medium" className="cursor-pointer hover:text-primary transition-colors" />
+      {/* <button className="bg-primary text-white px-3 py-1.5 md:px-4 md:py-2 rounded-md text-xs hover:bg-primary-dark transition-colors">My Resume</button> */}
+      <LinkedIn fontSize="large" className="cursor-pointer hover:text-primary transition-colors" 
+        onClick={() => window.open('https://www.linkedin.com/in/paul-sztwiertnia/', '_blank')}
+      />
+      <GitHub fontSize="large" className="cursor-pointer hover:text-primary transition-colors" 
+        onClick={() => window.open('https://github.com/paulsztwiertnia', '_blank')}
+      />
     </div>
   )
 }
